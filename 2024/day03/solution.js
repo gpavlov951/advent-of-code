fe76795d12b3edda8bbdc8fc63169989f64d0632
Georgi Pavlov <gpavlov951@gmail.com>
2024-12-11 23:21:49 +0100

    2024. Day 03. Part 2

--- 2024/day03/solution.js
+++ 2024/day03/solution.js
@@ -4,30 +4,75 @@ const path = require("path");
 const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
 
 // --- Part One ---
-const lengthOfValidSequence = 4; // mul(
-let resultsOfMultiplications = 0;
+const validSequence = "mul(";
+const lengthOfValidSequence = validSequence.length;
+let partOneResult = 0;
 
 for (let i = 0; i < input.length; i++) {
   const char = input[i];
 
-  if (char === "m" && input.slice(i, i + lengthOfValidSequence) === "mul(") {
-    const indexFirstBraket = i + 3;
-    const indexEndBraket = input.indexOf(")", i + lengthOfValidSequence);
-    const lengthValidString = 9; // (000,000)
+  if (
+    char === "m" &&
+    input.slice(i, i + lengthOfValidSequence) === validSequence
+  ) {
+    const res = getMultiplications(i);
+    if (!res) continue;
 
-    if (indexEndBraket - indexFirstBraket >= lengthValidString) continue;
-
-    const [firstNumber, secondNumber] = input
-      .slice(indexFirstBraket + 1, indexEndBraket)
-      .split(",")
-      .map(Number);
-
-    if (isNaN(firstNumber) || isNaN(secondNumber)) continue;
-
-    resultsOfMultiplications += firstNumber * secondNumber;
+    partOneResult += res;
   }
 }
 
-console.log("Results of Multiplications:", resultsOfMultiplications);
+/**
+ *
+ * @param {number} index
+ * @returns
+ */
+function getMultiplications(index) {
+  const indexFirstBraket = index + 3;
+  const indexEndBraket = input.indexOf(")", index + lengthOfValidSequence);
+  const lengthValidString = 9; // (000,000)
+
+  if (indexEndBraket - indexFirstBraket >= lengthValidString) return;
+
+  const [firstNumber, secondNumber] = input
+    .slice(indexFirstBraket + 1, indexEndBraket)
+    .split(",")
+    .map(Number);
+
+  if (isNaN(firstNumber) || isNaN(secondNumber)) return;
+
+  return firstNumber * secondNumber;
+}
+
+console.log("Results of Multiplications:", partOneResult);
 
 // --- Part Two ---
+
+let isEnable = true;
+let partTwoResult = 0;
+let nextTargetIndex = input.indexOf("don't()"); // First 'don't()' because we start with isEnable = true
+
+for (let i = 0; i < input.length; i++) {
+  const char = input[i];
+
+  if (nextTargetIndex === i) {
+    nextTargetIndex = isEnable
+      ? input.indexOf("do()", i)
+      : input.indexOf("don't()", i);
+    isEnable = !isEnable;
+  }
+
+  if (!isEnable) continue;
+
+  if (
+    char === "m" &&
+    input.slice(i, i + lengthOfValidSequence) === validSequence
+  ) {
+    const res = getMultiplications(i);
+    if (!res) continue;
+
+    partTwoResult += res;
+  }
+}
+
+console.log("Part Two Result:", partTwoResult);
--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Each year's solutions are stored in their own directory:
 | -------------------- | :----: | :----: | ---------------------------------- |
 | [Day 1](2024/day01/) |   ⭐   |   ⭐   | [Solution](2024/day01/solution.js) |
 | [Day 2](2024/day02/) |   ⭐   |   ☆    | [Solution](2024/day02/solution.js) |
-| [Day 3](2024/day03/) |   ⭐   |   ☆    | [Solution](2024/day03/solution.js) |
+| [Day 3](2024/day03/) |   ⭐   |   ⭐   | [Solution](2024/day03/solution.js) |
 
 **Total Stars: 4/50**
 
